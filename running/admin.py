e71c163b0286c607fce4dd911b1215cd4d4736ce
zy <qdzs0731@foxmail.com>
2021-03-23 19:34:51 +0800

    1.添加只读站点ReadOnlyAdmin

--- running/admin.py
+++ running/admin.py
@@ -1,20 +1,40 @@
 from django.contrib import admin
 from .models import Country, Province, Area, City
 
+
+class ReadOnlyAdmin(admin.ModelAdmin):
+    readonly_fields = []
+
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(self.readonly_fields) + \
+               [field.name for field in obj._meta.fields] + \
+               [field.name for field in obj._meta.many_to_many]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(ReadOnlyAdmin):
     search_fields = ('chn_name', 'eng_name',)
 
 
 @admin.register(Province)
-class ProvinceAdmin(admin.ModelAdmin):
-    search_fields = ('chn_name', 'eng_name', )
+class ProvinceAdmin(ReadOnlyAdmin):
+    search_fields = ('chn_name', 'eng_name',)
 
 
-class CityAdmin(admin.ModelAdmin):
+@admin.register(City)
+class CityAdmin(ReadOnlyAdmin):
+    autocomplete_fields = ['provinceid', 'countryid', ]
+
     list_display = ('cityid', 'countryid', 'areaid', 'provinceid', 'chn_name', 'eng_name')
-    autocomplete_fields = ['provinceid', 'countryid',]
-
-
-admin.site.register(Area)
-admin.site.register(City, CityAdmin)
